1f184e0bc2c3c7da326fd67f05fa36d5ba699ed3
John Silveira <john.silveira.pb@compasso.com.br>
2024-08-10 18:58:52 -0300

    ✨feat: tela de inicial funcionando

---
+++ trabalho_agenda/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root"; // ou o nome de usuário do banco de dados
+$password = ""; // ou a senha do banco de dados
+$dbname = "agenda";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+?>
